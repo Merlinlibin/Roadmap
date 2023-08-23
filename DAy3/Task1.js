6d77560f7ba54c6e268545a2659d2849c4e8dbe8
Merlinlibin <merlinlibinmerlin@gmail.com>
2023-08-23 11:38:59 +0530

    day 3 task

--- DAy3/Task1.js
+++ DAy3/Task1.js
@@ -1,6 +1,6 @@
 let obj1 = { name: 'person 1', age: 6, dob:3111996 };
 let obj2 = { dob:3111996 , age: 6, name: 'person 1'};
-let objEqual = false;
+
 const obj1Keys = Object.entries(obj1).sort();
 const obj2Keys = Object.entries(obj2).sort();
 
